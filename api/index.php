5c1367df3e804ef667fd2d2e691141b95e980b43
iwanharyatno <iwnharry61@gmail.com>
2024-12-02 14:12:43 +0700

    create homepage

--- api/index.php
+++ api/index.php
@@ -4,6 +4,7 @@ require "./db/config.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,23 +12,27 @@ require "./db/config.php";
     <link rel="stylesheet" href="/css/fonts.css">
     <link rel="stylesheet" href="/css/styles.css">
 </head>
+
 <body>
-    <header>
-        <nav>
-            <h1>SIMPAUD</h1>
-            <ul>
-                <li><a href="#">Beranda</a></li>
-                <li><a href="#">Tentang Kami</a></li>
-                <li><a href="#">Informasi Layanan</a></li>
-                <li><a href="#">Kontak</a></li>
+    <header class="header">
+        <nav class="navigation">
+            <h1 class="navigation-title"><span id="title-sim">SIM</span>PAUD</h1>
+            <ul class="navigation-menu">
+                <li><a class="navigation-item" href="/">Beranda</a></li>
+                <li><a class="navigation-item" href="#">Tentang Kami</a></li>
+                <li><a class="navigation-item" href="#">Informasi Layanan</a></li>
+                <li><a class="navigation-item nav-last-item" href="#">Kontak</a></li>
+                <li><a class="btn btn-primary" href="javascript:void(0)">Daftar</a></li>
             </ul>
         </nav>
+        <section class="hero">
+            <div class="hero-text">
+                <p class="hero-title">Tempat pilihan untuk <span id="hero-belajar">belajar</span> dan <span id="hero-bermain">bermain</span> untuk anak!</p>
+                <p class="hero-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, voluptatibus! Praesentium, adipisci!</p>
+                <a href="javascript:void(0)" class="btn btn-primary hero-btn">Ayo mulai!</a>
+            </div>
+        </section>
     </header>
-
-    <?php
-
-    echo $_SERVER["REQUEST_URI"];
-
-    ?>
 </body>
+
 </html>
