b1288c670e15bfcc6fafacaeff645b958cd6e840
iwanharyatno <iwnharry61@gmail.com>
2024-12-02 17:42:43 +0700

    reorganization

--- api/index.php
+++ api/index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIMPAUD - Sistem Informasi PAUD</title>
-    <link rel="stylesheet" href="/css/fonts.css">
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/fonts.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 
 <body>
